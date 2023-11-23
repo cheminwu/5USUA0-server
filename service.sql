21c301f26be3973c3fc26cd44040cf20a3a92ca4
chemin <2753056365@qq.com>
2023-11-23 15:54:52 +0100

    DB designing 1 version

--- service.sql
+++ service.sql
@@ -1,7 +1,28 @@
-create table demo
+create table user_info
 (
     id   int auto_increment
         primary key,
-    code varchar(20) not null,
-    flag varchar(20) not null
+    name varchar(20) not null,
+    birthday varchar(20) not null,
+    gender int not null
 );
+
+create table locker_info
+(
+    id   int auto_increment
+        primary key,
+    cabinet_id int not null,
+    item varchar(20) not null
+);
+
+
+
+create table locker_user_map
+(
+    id   int auto_increment
+        primary key,
+    locker_id int not null,
+    user_id int not null
+);
+
+
