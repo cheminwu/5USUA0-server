9a68aa21845ad7d8d627fef232b64de9c8fcf3af
chemin <2753056365@qq.com>
2023-12-13 01:24:47 +0100

    new

--- src/main/java/tue/student/iot/group18/mqttclient/Subscriber.java
+++ src/main/java/tue/student/iot/group18/mqttclient/Subscriber.java
@@ -1,13 +1,19 @@
 package tue.student.iot.group18.mqttclient;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tue.student.iot.group18.gym2go.Util;
+import tue.student.iot.group18.module.Request;
 import tue.student.iot.group18.service.ED25519;
 import tue.student.iot.group18.service.MD5;
+import tue.student.iot.group18.service.RequestService;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 
 @Service
 public class Subscriber {
@@ -18,12 +24,29 @@ public class Subscriber {
     @Autowired
     Publisher publisher;
 
+
+    @Autowired
+    RequestService requestService;
     @PostConstruct
     public void subscribe() {
         try{
             IMqttClient subscriber = Client.getClient();
             subscriber.subscribe("history", (topic, msg) -> {
                 System.out.println(msg);
+
+                JsonObject jsonObject = JsonParser.parseString(msg.toString())
+                        .getAsJsonObject();
+
+                Integer requestId = jsonObject.get("requestId").getAsInt();
+                String type = jsonObject.get("type").getAsString();
+                if("unlock".equals(type)){
+                    Request request = new Request();
+                    request.setId(requestId);
+                    request.setState(1);
+                    request.setUnlocktime(new Date());
+                    requestService.update(request);
+                }
+
             });
             subscriber.subscribe("start", (topic, msg) -> {
                 System.out.println(msg);
